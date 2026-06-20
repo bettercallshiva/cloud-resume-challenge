1fa71936d7b3bb3cac97f6cf9d4e35606da9ec76
shiva <shivashiva290306@gmail.com>
2026-06-20 13:53:25 +0530

    initial cloud resume project

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+function App() {
+  return (
+    <div>
+      <h1>Shiva</h1>
+      <h2>Cloud & DevOps Enthusiast</h2>
+
+      <p>
+        Learning AWS, Docker, Terraform and CI/CD while building
+        real-world cloud projects.
+      </p>
+    </div>
+  )
+}
+
+export default App
